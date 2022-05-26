ac2c779fdff7bb31866791eeda3f8b559bc07c7b
Rafael Borges <rafael.borges@luizalabs.com>
2022-05-26 19:32:04 -0300

    test class

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -5,13 +5,8 @@ from typing import Optional
 
 @dataclass
 class Category:
-    
+
     name:str
     description:Optional[str] = None
     is_active:Optional[bool] = True
     created_at:Optional[datetime] = field(default_factory=lambda: datetime.now())
-    
-    
-print(Category("filme 1"))
-print(Category("filme 2"))
-print(Category("filme 3"))
--- src/category/tests/unit/domain/test_unit_entities.py
+++ src/category/tests/unit/domain/test_unit_entities.py
@@ -1,4 +1,5 @@
 import unittest
+from dataclasses import is_dataclass
 from datetime import datetime
 
 from category.domain.entities import Category
@@ -6,11 +7,16 @@ from category.domain.entities import Category
 
 class TestCategoryUnitEntity(unittest.TestCase):
 
-    def test_constructor(self):
+    def test_if_it_is_a_dataclass(self):
+        category = Category('name', 'description', True, datetime.now())
 
+        self.assertTrue(is_dataclass(category))
+
+
+    def test_constructor_with_params_filled(self):
         name = 'name'
         description = 'description'
-        is_active = True
+        is_active = False
         created_at = datetime.now()
 
         category = Category(name, description, is_active, created_at)
@@ -19,3 +25,19 @@ class TestCategoryUnitEntity(unittest.TestCase):
         self.assertEqual(category.description, description)
         self.assertEqual(category.is_active, is_active)
         self.assertEqual(category.created_at, created_at)
+
+
+    def test_constructor_with_only_required_filled(self):
+        category = Category("name")
+
+        self.assertEqual(category.name, "name")
+        self.assertIsNone(category.description)
+        self.assertTrue(category.is_active)
+        self.assertIsInstance(category.created_at, datetime)
+
+
+    def test_created_at_must_have_different_time(self):
+        category1 = Category("name")
+        category2 = Category("name")
+
+        self.assertNotEqual(category1.created_at.timestamp(), category2.created_at.timestamp())
