285287f38e6a24d34f9ded55e5d89941bd618111
Rafael Borges <rafael.borges@luizalabs.com>
2022-05-28 15:22:26 -0300

    create value object

---
+++ src/__seedwork/domain/value_objects.py
@@ -0,0 +1,35 @@
+import abc
+import json
+import uuid
+from dataclasses import dataclass, field, fields
+
+from __seedwork.domain.exceptions import InvalidUuidException
+
+
+@dataclass(frozen=True)
+class ValueObject(abc.ABC):
+    def __str__(self) -> str:
+        fields_name = [field.name for field in fields(self)]
+
+        return str(getattr(self, fields_name[0])) \
+            if len(fields_name) == 1 \
+            else json.dumps({ field_name: getattr(self, field_name) for field_name in fields_name })
+
+
+@dataclass(frozen=True)
+class UniqueEntityId(ValueObject):
+
+    id:str = field(default_factory=lambda: str(uuid.uuid4()))
+
+    def __post_init__(self):
+        object.__setattr__(self, 'id', str(self.id))
+        self.__validate()
+
+    def __validate(self):
+        try:
+            uuid.UUID(self.id)
+        except ValueError as ex:
+            raise InvalidUuidException() from ex
+
+    # def __str__(self) -> str:
+    #     return f"{self.id}"
