8201a7ef7f5bc741e8edbfa084da85a1dfef9187
Rafael Borges <rafael.borges@luizalabs.com>
2022-05-30 14:06:57 +0000

    make Category extends Entity

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,13 +1,14 @@
-import uuid
 from dataclasses import dataclass, field
 from datetime import datetime
 from typing import Optional
 
+from __seedwork.domain.entities import Entity
+
+# from category.domain.entities import Category
 
 @dataclass(kw_only=True, frozen=True)
-class Category:
+class Category(Entity):
 
-    id:Optional[uuid.UUID] = field(default_factory=lambda: uuid.uuid4())
     name:str
     description:Optional[str] = None
     is_active:Optional[bool] = True
